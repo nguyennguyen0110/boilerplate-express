1b68cb08d0bed3b4aa836bd183f6ba46b32664c8
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-10-02 10:04:26 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+
+//Import and add middleware to for POST request (parse the POST body)
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 console.log('Hello World');
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', (req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -16,9 +17,11 @@ app.get('/', (req, res) => {
   res.sendFile(absolutePath);
 });
 
+//Use static folder to store static file for general use like css file
 const absStaticPath = __dirname + '/public';
 app.use('/public', express.static(absStaticPath));
 
+//The use of .env file (or secret in Replit)
 app.get('/json', (req, res) => {
   if (process.env['MESSAGE_STYLE'] == 'uppercase') {
     res.json({"message": "HELLO JSON"});
@@ -28,6 +31,7 @@ app.get('/json', (req, res) => {
   }
 });
 
+//Chaining callback functions with next()
 app.get('/now', (req, res, next) => {
   req.time = new Date().toString();
   next();
@@ -36,10 +40,12 @@ app.get('/now', (req, res, next) => {
   res.json({"time": req.time});
 });
 
+//Set and get the parameters from GET request
 app.get('/:word/echo', (req, res) => {
   res.json({"echo": req.params.word});
 });
 
+//Chaining GET and POST request for a route ***NOTE that POST needs body-parser
 app.route("/name").get((req, res) => {
   res.json({"name": req.query.first + " " + req.query.last});
 })
@@ -47,4 +53,4 @@ app.route("/name").get((req, res) => {
   res.json({"name": req.body.first + " " + req.body.last});
 });
 
-module.exports = app;
+ module.exports = app;
